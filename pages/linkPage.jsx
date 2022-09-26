a594593ac956c01cdbab4092809cb1a19faf2d50
Hiroya Shitogiden <dende.capo@gmail.com>
2022-09-26 16:12:28 +0900

    linkページひな形

--- pages/linkPage.jsx
+++ pages/linkPage.jsx
@@ -1,10 +1,17 @@
 import { memo } from "react";
+import Menu from "../components/menu";
+import styles from "./index.module.css";
 
 const linkPage = memo(() =>{
 
     return (
         <>
-        <p>coming soon</p>
+        <main className={styles.container}>
+        <Menu/>
+        <h1>Links page</h1>
+        <p>フォロワー様のプロフィールページやおすすめサイトへのリンク集です</p>
+        <p>掲載希望フォロワー様随時募集中。気軽にDMください⇒<a href="https://twitter.com/dendeiriamaka1">dende趣味Twitter</a></p>
+        </main>
         </>
 
 
