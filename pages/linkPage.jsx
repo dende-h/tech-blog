fdd549fed0571de630a0d360192b44ca0ed0b59d
Hiroya Shitogiden <dende.capo@gmail.com>
2022-09-26 17:22:26 +0900

    gridのレイアウトひな形設置

--- pages/linkPage.jsx
+++ pages/linkPage.jsx
@@ -1,6 +1,6 @@
 import { memo } from "react";
 import Menu from "../components/menu";
-import styles from "./index.module.css";
+import styles from "./linkPage.module.css";
 
 const linkPage = memo(() =>{
 
@@ -11,6 +11,13 @@ const linkPage = memo(() =>{
         <h1>Links page</h1>
         <p>フォロワー様のプロフィールページやおすすめサイトへのリンク集です</p>
         <p>掲載希望フォロワー様随時募集中。気軽にDMください⇒<a href="https://twitter.com/dendeiriamaka1">dende趣味Twitter</a></p>
+        <div className={styles.grid_parent}>
+            <div className={styles.grid_item}>1</div>
+            <div className={styles.grid_item}>2</div>
+            <div className={styles.grid_item}>3</div>
+            <div className={styles.grid_item}>4</div>
+            <div className={styles.grid_item}>5</div>
+        </div>
         </main>
         </>
 
