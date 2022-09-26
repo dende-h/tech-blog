a1f510e84fe15e650aded3df48c9e229911493a9
Hiroya Shitogiden <dende.capo@gmail.com>
2022-09-26 15:48:45 +0900

    publicにイラスト配置で自動画像配置

--- pages/illustration.jsx
+++ pages/illustration.jsx
@@ -2,15 +2,23 @@ import { memo } from "react";
 import Menu from "../components/menu";
 import ArticleImg from "../components/articleImg";
 import styles from "./illustration.module.css";
+import { commentsArray } from "../lib/comment";
 
-const illustration = memo(() =>{
+
+
+const illustration = memo(({comments, posts}) =>{
+
+    
 
     return (
         <>
-            <main className={styles.container}>
-                <Menu/>
-                <ArticleImg text={"こんにちは"} imgUrl={"/fight.png"} />
-            </main>
+        <main className={styles.container}>
+        <Menu/>
+        <h1>Dende's illustration place</h1>
+        <p>Twitterでupしているオリジナルイラストや二次創作イラストを載せています</p>
+        {posts.map((fileName,index)=>{return <ArticleImg text={comments[index]} imgUrl={`/${fileName}`} /> })
+           }
+           </main>
         </>
 
 
@@ -20,3 +28,17 @@ const illustration = memo(() =>{
 
 })
 export default illustration
+
+export const getStaticProps = async () => {
+    const glob = require('glob');
+    const files = glob.sync( "./public/*.{jpg,png}");
+    const fileNames = files.map((file)=>{ return file.split("/").pop()})
+    const comments = commentsArray
+    return {
+      props: {
+        comments:comments,
+        posts: fileNames,
+      },
+      revalidate: 1,
+    };
+  };
