1187555be59712cc1a199cecd5cea6d0dc9e3e80
dende-h <miyuki_dende@yahoo.co.jp>
2022-10-01 06:50:43 +0900

    googleアナリティクスのページビュー観測を実装修正

--- hooks/usePageView.jsx
+++ hooks/usePageView.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useRouter } from "next/router";
 
-import { existsGaId, pageView } from '../lib/gtag'
+import { existsGaId, pageview } from '../lib/gtag'
 
 export default function usePageView() {
   const router = useRouter()
@@ -12,7 +12,7 @@ export default function usePageView() {
     }
 
     const handleRouteChange = (path) => {
-      pageView(path)
+      pageview(path)
     }
 
     router.events.on('routeChangeComplete', handleRouteChange)
